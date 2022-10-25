69a71c3e8b891c81f8202b7ed69282b5b42f97dd
cellle <91531988+cellle@users.noreply.github.com>
2022-10-25 09:57:35 +0200

    footer wip

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <img src="" alt="" />
+      <div></div>
+      <p>Layout By Jit Banik 2020</p>
+    </footer>
+  );
+}
+
+export default Footer;
