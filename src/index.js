b08787d2a67cf71d9005192de3cb1f5490003e28
Kim <howtoad123@gmail.com>
2022-10-25 13:03:32 +0200

    sets up hero part of front page. Creates Button component, Sets up searchbox for hero part of front page. Creates Home and adds an image folder with hte images into src

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,7 +11,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
