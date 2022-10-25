a759bd4d732b386e40505f03ab45f77d6b15f17c
cellle <91531988+cellle@users.noreply.github.com>
2022-10-25 12:36:57 +0200

    import footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       {/* <Header/> */}
       <Outlet />
-      {/* <Footer/> */}
+      <Footer />
     </div>
   );
 }
