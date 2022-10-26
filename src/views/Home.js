6ede17e4f3130c8862af6e6ce1fdd53ef3059731
cellle <91531988+cellle@users.noreply.github.com>
2022-10-26 13:45:37 +0200

    implement sales app section

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,11 @@
 import HeroImg from "../templates/HeroImg";
+import SalesAppSection from "../templates/SalesAppSection";
 
 const Home = () => {
   return (
     <>
       <HeroImg />
+      <SalesAppSection />
     </>
   );
 };
