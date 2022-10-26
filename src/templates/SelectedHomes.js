3d2282f7885fe1591e0b36b25ca285e0d0fac0c2
cellle <91531988+cellle@users.noreply.github.com>
2022-10-26 14:17:19 +0200

    implement selected homes wip

---
+++ src/templates/SelectedHomes.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SelectedHomes() {
+  return (
+    <div className="bg-background">
+      <div className="max-w-[1100px] mx-auto flex flex-col items-center">
+        <h2 className="text-3xl font-bold text-heading">Udvalgte Boliger</h2>
+      </div>
+    </div>
+  );
+}
+
+export default SelectedHomes;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,11 @@
 import HeroImg from "../templates/HeroImg";
+import SelectedHomes from "../templates/SelectedHomes";
 
 const Home = () => {
   return (
     <>
       <HeroImg />
+      <SelectedHomes />
     </>
   );
 };
