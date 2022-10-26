074e2b7e91eee0a543bba17f7135d282a7f3208e
cellle <91531988+cellle@users.noreply.github.com>
2022-10-26 14:17:30 +0200

    home component wip

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home({ showLiked }) {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h3></h3>
+      <p></p>
+      <p></p>
+    </div>
+  );
+}
+
+export default Home;
