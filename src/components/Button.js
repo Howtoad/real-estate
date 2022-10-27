2bfd0d5f5a16dda7f8bdfda37a348fe714156c0b
Kim <howtoad123@gmail.com>
2022-10-27 08:51:52 +0200

    Standard button component set up

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,9 @@
+//The primary button used on the site.
+//Has height and text color, change the bg color and max-w-[] when in use to fit your needs.
 const Button = ({ children, css }) => {
-  return <button className={css}>{children}</button>;
+  return (
+    <button className={css + " " + "h-12 text-white w-full"}>{children}</button>
+  );
 };
 
 export default Button;
