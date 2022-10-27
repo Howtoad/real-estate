f067be6ecfe9862f88465818be5066759d5f72bb
cellle <91531988+cellle@users.noreply.github.com>
2022-10-27 12:56:23 +0200

    fix warning

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -7,10 +7,7 @@ const HeaderNav = () => {
     <div className="flex w-full h-36 place-content-center">
       <div className="flex max-w-[1100px] place-self-center w-full">
         <NavLink to="/">
-          <img
-            src="/images\real-estate\logo.png"
-            alt="Image of the company logo"
-          ></img>
+          <img src="/images\real-estate\logo.png" alt="Company logo"></img>
         </NavLink>
         <div className="ml-auto place-self-end flex space-x-10">
           <NavLink to="/boliger" className={styles.headerLink}>
