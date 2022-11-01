7cdee05c02a467afa8bf14436c6c6d3bba7d47ac
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-11-01 12:31:54 +0100

    add modal window, with plan and location content

---
+++ src/features/PropertyDetails/components/ModalWindow.js
@@ -0,0 +1,112 @@
+import { motion, useAnimationControls } from "framer-motion";
+import { useEffect, useCallback, useState } from "react";
+
+// component requires a useState called showModalWindow and setShowModalWindow to be passed down
+const ModalWindow = ({
+  showModalWindow,
+  setShowModalWindow,
+  modalContainerAnimation,
+  modalContentAnimation,
+  children,
+}) => {
+  //default animation
+  const ContainerAnimation = {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 0.5,
+    },
+  };
+  const ContentAnimation = {
+    hidden: {
+      display: "none",
+      opacity: 0,
+    },
+    show: {
+      display: "flex",
+      opacity: 1,
+    },
+  };
+
+  const containerControls = useAnimationControls();
+  const contentControls = useAnimationControls();
+  const [ariaHidden, setAriaHidden] = useState(false);
+
+  const showSequence = async () => {
+    await containerControls.start("show");
+    return await contentControls.start("show");
+  };
+  const hideSequence = async () => {
+    contentControls.start("hidden");
+    containerControls.start("hidden");
+  };
+
+  useEffect(() => {
+    if (showModalWindow === true) {
+      setAriaHidden(false);
+    } else {
+      setAriaHidden(true);
+    }
+  }, [showModalWindow]);
+
+  if (showModalWindow === true) {
+    showSequence();
+  } else {
+    hideSequence();
+  }
+
+  const escapeDown = useCallback((event) => {
+    if (event.keyCode === 27) {
+      setShowModalWindow(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", escapeDown, false);
+    return () => {
+      document.removeEventListener("keydown", escapeDown, false);
+    };
+  }, []);
+
+  const styles = {
+    button: "max-w-max max-h-max bg-gray-500 rounded m-1 p-1 text-xl",
+  };
+
+  return (
+    <>
+      <motion.section
+        aria-modal="true"
+        role="dialog"
+        aria-hidden={ariaHidden}
+        className="lightbox w-screen h-screen bg-black fixed top-0"
+        variants={
+          (modalContainerAnimation && modalContainerAnimation) ||
+          ContainerAnimation
+        }
+        initial="hidden"
+        animate={containerControls}
+        onClick={(e) => {
+          e.target === e.currentTarget && setShowModalWindow(false);
+        }}
+      ></motion.section>
+      <motion.div
+        aria-modal="true"
+        role="alertdialog"
+        aria-hidden={ariaHidden}
+        className="m-auto min-w-10 min-h-10 w-fit h-fit z-10 bg-white fixed top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 justify-between flex-col rounded-md"
+        variants={
+          (modalContentAnimation && modalContentAnimation) || ContentAnimation
+        }
+        initial="hidden"
+        animate={contentControls}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
+};
+
+export default ModalWindow;
--- src/features/PropertyDetails/templates/DetailsHeader.js
+++ src/features/PropertyDetails/templates/DetailsHeader.js
@@ -1,32 +1,86 @@
 import { IoImageOutline, IoLocationOutline } from "react-icons/io5";
 import { AiOutlineHeart } from "react-icons/ai";
 import { RiStackLine } from "react-icons/ri";
+import ModalWindow from "../components/ModalWindow";
+import { useState } from "react";
 
 const DetailsHeader = ({ houseInfo }) => {
-  console.log(houseInfo);
+  const [showModalWindow, setShowModalWindow] = useState();
+  const [modalContent, setModalContent] = useState();
 
   const iconCss = "w-auto h-[50px] text-gray-500 mx-4 paragraph_2";
-  return (
-    <div className="mx-auto mt-[39px] w-full max-w-[1110px] flex justify-between border-solid border-b-2">
-      <div className="mb-4">
-        <h2 className="font-bold mb-2 text-lg text-primary">
-          {houseInfo?.adress1} {houseInfo?.adress2}
-        </h2>
-        <h2 className="font-bold text-lg text-primary">
-          {houseInfo?.postalcode} {houseInfo?.city}
-        </h2>
-      </div>
-      <div className="flex">
-        <IoImageOutline className={iconCss} />
-        <RiStackLine className={iconCss} />
-        <IoLocationOutline className={iconCss} />
-        <AiOutlineHeart className={iconCss} />
-      </div>
-      <h3 className="font-bold text-2xl text-primary">
-        Kr. &nbsp;
-        {houseInfo?.price ? houseInfo?.price.toLocaleString() : "--"}
-      </h3>
+  const buildingPlan = (
+    <div>
+      <img src={houseInfo?.floorplan?.url} alt="Floorplan" />
     </div>
+  );
+
+  const map = (
+    <>
+      <iframe
+        src={
+          "https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d9010.26635989766!2d" +
+          houseInfo?.long +
+          "!3d" +
+          houseInfo?.lat +
+          "!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sdk!4v1667300649685!5m2!1sen!2sdk"
+        }
+        width="800"
+        height="600"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
+  );
+  return (
+    <>
+      <ModalWindow
+        showModalWindow={showModalWindow}
+        setShowModalWindow={setShowModalWindow}
+      >
+        {modalContent}
+      </ModalWindow>
+      <div className="mx-auto mt-[39px] w-full max-w-[1110px] flex justify-between border-solid border-b-2">
+        <div className="mb-4">
+          <h2 className="font-bold mb-2 text-lg text-primary">
+            {houseInfo?.adress1} {houseInfo?.adress2}
+          </h2>
+          <h2 className="font-bold text-lg text-primary">
+            {houseInfo?.postalcode} {houseInfo?.city}
+          </h2>
+        </div>
+        <div className="flex">
+          <IoImageOutline
+            className={iconCss}
+            onClick={() => {
+              setShowModalWindow(true);
+              setModalContent();
+            }}
+          />
+          <RiStackLine
+            className={iconCss}
+            onClick={() => {
+              setShowModalWindow(true);
+              setModalContent(buildingPlan);
+            }}
+          />
+          <IoLocationOutline
+            className={iconCss}
+            onClick={() => {
+              setShowModalWindow(true);
+              setModalContent(map);
+            }}
+          />
+          <AiOutlineHeart className={iconCss} />
+        </div>
+        <h3 className="font-bold text-2xl text-primary">
+          Kr. &nbsp;
+          {houseInfo?.price ? houseInfo?.price.toLocaleString() : "--"}
+        </h3>
+      </div>
+    </>
   );
 };
 
