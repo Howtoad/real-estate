6eb3f376e41711c4975268b49799e88c15749f83
Kim <howtoad123@gmail.com>
2022-11-02 10:26:08 +0100

    first box with agent info done on agent_details

--- src/templates/ContactAgentTemplate.js
+++ src/templates/ContactAgentTemplate.js
@@ -1,13 +1,28 @@
 import AgentInfo from "../components/AgentInfo";
-import useFetchAgent from "../customHooks/useFetchAgent";
+import useFetchAgent from "../hooks/useFetchAgent";
 
 const ContactAgentTemplate = () => {
   const { content } = useFetchAgent();
   console.log(content);
   return (
-    <div className="max-w-[730px] mx-auto">
-      <div className="mx-auto">
+    <div className="max-w-[730px] border mt-28">
+      <div>
         <AgentInfo agent={content} />
+      </div>
+      <div className="px-10">
+        <h3 className="text-heading_2 text-xl font-medium">
+          Om {content?.name}
+        </h3>
+        <div className="w-10 h-1 bg-primary mb-7 mt-1"></div>
+        <p className="font-lg text-paragraph pb-5">
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+        </p>
+        <p className="font-lg text-paragraph">
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form by injected humour.
+        </p>
       </div>
     </div>
   );
