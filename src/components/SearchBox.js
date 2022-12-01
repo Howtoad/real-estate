34a56e0dcffbb6055b36b62ed5caf1da18b644dc
Alexander-Gronninger <93522244+Alexander-Gronninger@users.noreply.github.com>
2022-12-01 11:23:29 +0100

    remove warning

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Button from "./Button";
 
 const SearchBox = () => {
   const [query, setQuery] = useState(null);
